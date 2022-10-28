2281cda89aef6e96245b7b66bf67da4cf152acfe
Zaid Almuflahi <zaid.almuflahi1217@gmail.com>
2022-10-28 18:34:43 -0400

    commit tree 72ae61077876f41b49ce63cc0276648683290c5c

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(data => renderBooks(data));
 }
-
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
@@ -12,6 +14,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
